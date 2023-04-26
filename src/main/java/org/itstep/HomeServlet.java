c822ffaa4517a0efd341050826a547488f380101
AlexSemenyuk <115478573+AlexSemenyuk@users.noreply.github.com>
2023-04-26 11:39:54 +0300

    ready for check

---
+++ src/main/java/org/itstep/HomeServlet.java
@@ -0,0 +1,19 @@
+package org.itstep;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(req, resp);
+    }
+
+}
